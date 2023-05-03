f3439b1bda79a4c5f5d6e9ad9e787dd9d0f5d03c
DonTraffic <dogerfantom6@gmail.com>
2023-05-03 12:45:34 +0300

    small fix

---
+++ scripts/slider.js
@@ -0,0 +1,39 @@
+document.querySelectorAll('.slider').forEach(slider => {
+    const items = slider.querySelectorAll('.slider__line-item')
+    const sliderLine = slider.querySelector('.slider__line')
+    const sliderCount = slider.getAttribute('items-count') ? slider.getAttribute('items-count') : 1 ;
+
+    let count = 0
+    let width = 0
+
+    function init () {
+        width = slider.offsetWidth
+
+        sliderLine.style.width = `${(width * items.length) / sliderCount}px`
+        items.forEach( item => {
+            item.style.width = `${width / sliderCount}px`
+        })
+
+        rollSlider()
+    }
+
+    window.addEventListener('resize', init)
+    init()
+
+    function rollSlider() {
+        sliderLine.style.transform = `translate(-${count * width}px)`
+    }
+
+    slider.querySelector('.slider__btn-prev').addEventListener('click', e => {
+        count--
+        if (count < 0) count = (items.length / sliderCount) - 1 
+        rollSlider()
+    })
+
+    slider.querySelector('.slider__btn-next').addEventListener('click', e => {
+        count++
+        if (count >= (items.length / sliderCount)) count = 0
+        rollSlider()
+    })
+
+})
