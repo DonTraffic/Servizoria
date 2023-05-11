3bf20b7a3a217c4bc5850394faa951342f51dce2
DonTraffic <dogerfantom6@gmail.com>
2023-05-12 01:33:28 +0300

    add function adaptation slider

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,8 +1,10 @@
 document.querySelectorAll('.slider').forEach(slider => {
+
     const items = slider.querySelectorAll('.slider__line-item')
+    const setting = slider.getAttribute('items-count') ?
+        JSON.parse(slider.getAttribute('items-count')) : {0: 1}
     const sliderLine = slider.querySelector('.slider__line')
     const sliderWrap = slider.querySelector('.slider__line-wrap')
-    const sliderCount = slider.getAttribute('items-count') ? slider.getAttribute('items-count') : 1 ;
 
     let count = 0
     let width = 0
@@ -10,7 +12,7 @@ document.querySelectorAll('.slider').forEach(slider => {
     function init () {
         width = sliderWrap.offsetWidth
 
-        sliderLine.style.width = `${(width * items.length) / sliderCount}px`
+        sliderLine.style.width = `${(width * items.length) / sliderCount()}px`
         items.forEach( item => {
             item.style.width = `${width / sliderCount}px`
         })
@@ -21,19 +23,31 @@ document.querySelectorAll('.slider').forEach(slider => {
     window.addEventListener('resize', init)
     init()
 
+    function sliderCount() {
+        let count
+
+        for (const key in setting) {
+            if (!count && window.innerWidth <= key) {
+                count = setting[key]
+            }
+        }
+
+        return count ? count : setting[0]
+    }
+
     function rollSlider() {
         sliderLine.style.transform = `translate(-${count * width}px)`
     }
 
     slider.querySelector('.slider__btn-prev').addEventListener('click', e => {
         count--
-        if (count < 0) count = (items.length / sliderCount) - 1 
+        if (count < 0) count = (items.length / sliderCount()) - 1 
         rollSlider()
     })
 
     slider.querySelector('.slider__btn-next').addEventListener('click', e => {
         count++
-        if (count >= (items.length / sliderCount)) count = 0
+        if (count >= (items.length / sliderCount())) count = 0
         rollSlider()
     })
 
