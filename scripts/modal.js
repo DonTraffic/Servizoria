f312345a40169c32d5bd10e5d7b3f8faa5c0a651
DonTraffic <dogerfantom6@gmail.com>
2023-07-12 12:25:21 +0300

    update form modal, add phone mask and push bitrix

--- scripts/modal.js
+++ scripts/modal.js
@@ -31,6 +31,23 @@ function formSubmit(name, func) {
     })
 
     if (status) {
+
+        let link = ''
+        inputs.forEach( (input) => {
+            link = `${link}&fields[${input.getAttribute('name')}]=${input.value}`
+        })
+
+        fetch(`https://servizoria.bitrix24.ru/rest/835/dlbzb506kph50iby/crm.lead.add.json?fields[TITLE]=Новый лид Сервизория${link}&fields[SOURCE_ID]=Заявка с сайта`, {
+            method: 'POST',
+        }).then(response => {
+            if (response.ok) {
+                console.log('Данные успешно отправлены!');
+                return response.json();
+            } else {
+                throw new Error('Произошла ошибка при отправке данных.');
+            }
+        })
+        
         openModal('complite')
         closeModal(name)
         setTimeout(() => { closeModal('complite') }, 2000)
@@ -45,3 +62,17 @@ function formSubmit(name, func) {
     }
 
 }
+
+document.querySelectorAll('.modal__form-input-phone').forEach( input => {
+    input.addEventListener('input', e => {
+        let x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+        e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
+    });
+})
+
+// document.querySelectorAll('.modal__form-input-phone').forEach( input => {
+//     input.addEventListener('input', e => {
+//         let x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+//         e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
+//     });
+// })
